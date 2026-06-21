05a3137fa90fc5ebeb0f0b8a0275a2a87fb30101
shamsadanam <shamsad.anam@gmail.com>
2026-06-21 11:18:16 +0600

    feat: add api.ts and useApi.ts, update: folder structure as per nuxt 4 recomendation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/image", "@nuxt/ui", "@vueuse/nuxt"],
+  modules: ["@nuxt/image", "@nuxt/ui", "@vueuse/nuxt", "nuxt-auth-utils"],
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE,
+      enableProxy: process.env.enableProxy,
+      xxx: process.env.xxx,
+    },
+  },
 });
